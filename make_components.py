4d27058507a53ce3ce1bd1b25197d60291e527be
GenThrive <genthrive@ecorise.org>
2025-02-06 11:26:51 -0600

    added the new data and changed some fonts

--- make_components.py
+++ make_components.py
@@ -210,7 +210,7 @@ def make_map(orgdata, lat_col, lon_col, choro_geojson,  featureidkey, state_name
                zoom=map_zoom,
               center = {"lat": map_center_lat, "lon": map_center_lon},
               mapbox_style="open-street-map")
-    fig.update_traces(hovertemplate=f'<b>%{{{state_name}}}</b>')
+    fig.update_traces(hovertemplate=f'<b style="font-size:20px">%{{{state_name}}}</b>')
 
     # add pt layer to map
     for item in range(0,len(scatter_fig.data)):
